5993cc40f0e7866db0a4a08ab961a9c07e60b9a5
ArkahaSoro <107117533+ArkahaSoro@users.noreply.github.com>
2024-11-07 23:07:24 +0300

    Update neiro1.py

--- neiro1.py
+++ neiro1.py
@@ -4,24 +4,23 @@ from PIL import Image
 import torch
 from transformers import AutoImageProcessor, AutoModelForImageClassification
 
-#загрузка моделей
-@st.cache(allow_output_mutation = True)
+# Загрузка моделей
+@st.cache_resource(allow_output_mutation=True)
 def load_auto_image_processor():
-  return AutoImageProcessor.from_pretrained('chrimaue/bird-species-classifier')
+    return AutoImageProcessor.from_pretrained('chrimaue/bird-species-classifier')
 
-@st.cache(allow_output_mutation = True)
+@st.cache_resource(allow_output_mutation=True)
 def load_auto_model():
-  return AutoModelForImageClassification.from_pretrained('chrimae/bird-species-classifer')
+    return AutoModelForImageClassification.from_pretrained('chrimaue/bird-species-classifier')
 
-#инициализация процессора и модели
+# Инициализация процессора и модели
 processor = load_auto_image_processor()
 model = load_auto_model()
 
-#реализация функции распознания объектов на изображении
+# Реализация функции распознания объектов на изображении
 def predict_step(image):
     if image.mode != "RGB":
         image = image.convert(mode="RGB")
-    
     pixel_values = processor(images=image, return_tensors="pt").pixel_values
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     pixel_values = pixel_values.to(device)
@@ -29,23 +28,21 @@ def predict_step(image):
     
     with torch.no_grad():
         outputs = model(pixel_values)
-    
-    logits = outputs.logits
+        logits = outputs.logits
+        
     predicted_class_idx = logits.argmax(-1).item()
     labels = processor.config.id2label
     predicted_label = labels[predicted_class_idx]
     
     return predicted_label 
 
-#использование в стримлит
+# Использование в Streamlit
 st.title("Bird Species Classifier")
-uploaded_file = st.file_uploader("Выбирает изображение...", type =["jpg","jpeg","png"])
-
+uploaded_file = st.file_uploader("Выберите изображение...", type=["jpg", "jpeg", "png"])
 if uploaded_file is not None:
-  image = Image.open(uploaded_file)
-  st.image(image, caption='Upload Image.', use_column_width = True)
-  st.write("")
-  st.write("определяет вид...")
-
-  prediction = predict_step(image)
-  st.write(f"Относится к виду:{prediction}")
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    st.write("")
+    st.write("Определяет вид...")
+    prediction = predict_step(image)
+    st.write(f"Относится к виду: {prediction}")
