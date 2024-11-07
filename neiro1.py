11ae4e51799fbb1c3c58c71247ee1dcf863854c5
ArkahaSoro <107117533+ArkahaSoro@users.noreply.github.com>
2024-11-07 23:15:29 +0300

    Update neiro1.py

--- neiro1.py
+++ neiro1.py
@@ -19,30 +19,38 @@ model = load_auto_model()
 
 # Реализация функции распознания объектов на изображении
 def predict_step(image):
-    if image.mode != "RGB":
-        image = image.convert(mode="RGB")
-    pixel_values = processor(images=image, return_tensors="pt").pixel_values
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    pixel_values = pixel_values.to(device)
-    model.to(device)
-    
-    with torch.no_grad():
-        outputs = model(pixel_values)
-        logits = outputs.logits
-        
-    predicted_class_idx = logits.argmax(-1).item()
-    labels = processor.config.id2label
-    predicted_label = labels[predicted_class_idx]
-    
-    return predicted_label 
+    try:
+        if image.mode != "RGB":
+            image = image.convert(mode="RGB")
+        pixel_values = processor(images=image, return_tensors="pt").pixel_values
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        pixel_values = pixel_values.to(device)
+        model.to(device)
+
+        with torch.no_grad():
+            outputs = model(pixel_values)
+            logits = outputs.logits
+
+        predicted_class_idx = logits.argmax(-1).item()
+        labels = processor.config.id2label
+        predicted_label = labels[predicted_class_idx]
+
+        return predicted_label
+    except Exception as e:
+        st.error(f"Произошла ошибка при предсказании: {e}")
+        return None
 
 # Использование в Streamlit
 st.title("Bird Species Classifier")
 uploaded_file = st.file_uploader("Выберите изображение...", type=["jpg", "jpeg", "png"])
+
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image.', use_column_width=True)
     st.write("")
     st.write("Определяет вид...")
+    
     prediction = predict_step(image)
-    st.write(f"Относится к виду: {prediction}")
+    if prediction is not None:
+        st.write(f"Относится к виду: {prediction}")
+
