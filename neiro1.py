f52807d9f77280f9ac178240b55fe46b62672f7a
ArkahaSoro <107117533+ArkahaSoro@users.noreply.github.com>
2024-11-07 22:49:09 +0300

    Update neiro1.py

--- neiro1.py
+++ neiro1.py
@@ -19,19 +19,23 @@ model = load_auto_model()
 
 #реализация функции распознания объектов на изображении
 def predict_step(image):
-  if image.mode != "RGB":
-    image = image.convert(mode="RGB")
-pixel_values = processor(images = image, return_tensors="pt").pixel_values
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-pixel_values = pixel_values.to(device)
-model.to(device)
-with torch.no_grad():
-  outputs = model(pixel_values)
-logits = outputs.logits
-predicted_class_idx = logits.argmax(-1).item()
-labels = processor.config.id2label
-predicted_label = labels[predicted_class_idx]
-return predicted_label
+    if image.mode != "RGB":
+        image = image.convert(mode="RGB")
+    
+    pixel_values = processor(images=image, return_tensors="pt").pixel_values
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    pixel_values = pixel_values.to(device)
+    model.to(device)
+    
+    with torch.no_grad():
+        outputs = model(pixel_values)
+    
+    logits = outputs.logits
+    predicted_class_idx = logits.argmax(-1).item()
+    labels = processor.config.id2label
+    predicted_label = labels[predicted_class_idx]
+    
+    return predicted_label 
 
 #использование в стримлит
 st.title("Bird Species Classifier")
